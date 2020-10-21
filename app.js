fb1c4a39d8524fa0a40b4c22fc00ede67a332898
Aleksey Gorodbin <89o34681381@mail.ru>
2020-10-21 17:44:47 +0300

    ff

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const { errors } = require('celebrate');
 const {
   routerUsersId,
@@ -41,6 +42,7 @@ app.get('/crash-test', () => {
     throw new Error('Сервер сейчас упадёт');
   }, 0);
 });
+app.use(cors());
 app.post('/signin', login);
 app.post('/signup', changeUser);
 app.use(auth);
